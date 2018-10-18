454879e485b44dfdd9a24ee9959cd968fca35adf
TheFinnishArmy <thefinnisharmy@gmail.com>
2018-10-18 20:35:48 +0300

    Addition of a (non working) eval-ish function, I'll have to see about correcting it...

--- Main.py
+++ Main.py
@@ -1,9 +1,13 @@
+import base64
+import os
+import uuid
+from math import fmod
+
 import discord
 import asyncio
 import hashlib
 
 client = discord.Client()
-
 
 @client.event
 async def on_ready():
@@ -15,18 +19,15 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!test'):
+    if message.content.startswith('wh!count'):
         counter = 0
         tmp = await client.send_message(message.channel, 'Calculating messages...')
         async for log in client.logs_from(message.channel, limit=100):
             if log.author == message.author:
                 counter += 1
-
         await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith('!sleep'):
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
-    elif message.content.startswith('!hash'):
+
+    elif message.content.startswith('wh!hash'):
         hashable_message = message.content
         error = False
         try:
@@ -35,9 +36,25 @@ async def on_message(message):
             await client.send_message(message.channel, 'Please input string to be hashed as second parameter')
             error = True
         if not error:
-            await client.send_message(message.channel, 'SHA3-512 Hash of the message:')
+            await client.send_message(message.channel, 'SHA3-512 Hash of {}:'.format(b))
             await client.send_message(message.channel, hashlib.sha3_512(b.encode('utf-8')).hexdigest())
-    elif message.content.startswith('!shutdown') or message.content.startswith('!stop'):
+
+    elif message.content.startswith('wh!eval'):
+        secret = 'TestSecret'
+        nonce = uuid.uuid4().hex
+        await client.send_message(message.channel,
+                                  'Please calculate the hash of the secret and nonce: \n {} \n You have 30 seconds'
+                                  .format(nonce))
+        msg = await client.wait_for_message(author=message.author, timeout=30)
+        if msg is not None:
+            if msg is hashlib.sha3_512(secret.encode('utf-8') + nonce.encode('utf-8')).hexdigest():
+                await client.send_message(message.channel, 'You would\'ve gotten eval, it\'s not implemented though.')
+            else:
+                await client.send_message(message.channel, 'You sent an invalid hash or you timed out.')
+        else:
+            await client.send_message(message.channel, 'You sent an invalid hash or you timed out.')
+
+    elif message.content.startswith('wh!shutdown') or message.content.startswith('wh!stop'):
         await client.send_message(message.channel, 'Shutting down')
         await client.logout()
 
