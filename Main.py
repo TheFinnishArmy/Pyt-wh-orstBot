9bfc63bff4b63c5e8fb728eba7be8fd961da32b1
TheFinnishArmy <thefinnisharmy@gmail.com>
2018-10-18 22:45:42 +0300

    Discord Presence implementation TO-DO: Find how to activate "Streaming", possibly needs an url

--- Main.py
+++ Main.py
@@ -9,12 +9,16 @@ import hashlib
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
     print('------')
+
+    testGame = discord.Game(name="my creators ramblings", type=2)
+    await client.change_presence(game=testGame)
 
 
 @client.event
@@ -39,7 +43,43 @@ async def on_message(message):
             await client.send_message(message.channel, 'SHA3-512 Hash of {}:'.format(b))
             await client.send_message(message.channel, hashlib.sha3_512(b.encode('utf-8')).hexdigest())
 
+    elif message.content.startswith('wh!changePresence'):
+        hashable_message = message.content
+        error = False
+        a, b, c = None, None, None
+        try:
+            try:
+                a, b, c = hashable_message.split(' ')
+            except ValueError:
+                a, b = hashable_message.split(' ')
+        except ValueError:
+            await client.send_message(message.channel, 'Please input string to be used as the game')
+            error = True
+
+        if not error:
+            error = False
+            try:
+                cInt = int(c)
+            except ValueError:
+                await client.send_message(message.channel,
+                                          'Please enter a int as the second argument, current entered has been '
+                                          'discarded as it couldn\'t be converted')
+                error = True
+            except TypeError:
+                error = True
+
+            if not error:
+                testGame = discord.Game(name=b, type=cInt)
+                await client.change_presence(game=testGame)
+                await client.send_message(message.channel, 'Changed to status: {0} \n and game: {1}'.format(c, b))
+            else:
+                testGame = discord.Game(name=b)
+                await  client.change_presence(game=testGame)
+                await client.send_message(message.channel, 'Changed to game: {}'.format(b))
+
     elif message.content.startswith('wh!eval'):
+        await client.send_message(message.channel, 'Currently not working, @ my creator if you really want it.')
+        '''
         secret = 'TestSecret'
         nonce = uuid.uuid4().hex
         await client.send_message(message.channel,
@@ -53,6 +93,7 @@ async def on_message(message):
                 await client.send_message(message.channel, 'You sent an invalid hash or you timed out.')
         else:
             await client.send_message(message.channel, 'You sent an invalid hash or you timed out.')
+        '''
 
     elif message.content.startswith('wh!shutdown') or message.content.startswith('wh!stop'):
         await client.send_message(message.channel, 'Shutting down')
